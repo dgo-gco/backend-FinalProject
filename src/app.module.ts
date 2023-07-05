e0d7978549269ecec70db91fcfb63fee2d936e88
dgo-gco <jdiegogarciaco@gmail.com>
2023-07-05 12:01:07 +0200

    connect db deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-//import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -13,7 +13,7 @@ import { CommentsModule } from './comments/comments.module';
 @Module({
   imports: [
     UsersModule,
-    //ConfigModule.forRoot({ envFilePath: `local.env` }),
+    ConfigModule.forRoot({ envFilePath: `.env` }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
