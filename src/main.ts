099f055f1b8abe5a3760e33726b2a1c194839893
dgo-gco <jdiegogarciaco@gmail.com>
2023-07-05 13:18:59 +0200

    back to test in dev

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Mongoose } from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const corsOptions = {
-    origin: 'https://fantastic-trifle-538811.netlify.app/',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
   };
   app.setGlobalPrefix('api');
   app.enableCors(corsOptions);
   app.use(cookieParser());
-
-  const mongooseInstance = app.get<Mongoose>(Mongoose);
-  const mongooseConnection = mongooseInstance.connection;
-  mongooseConnection.once('connected', () => {
-    app.listen(process.env.PORT, () => {
-      console.log('Application is listening on port', process.env.PORT);
-    });
-  });
+  await app.listen(process.env.PORT);
 }
 bootstrap();
